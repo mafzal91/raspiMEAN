3464efb278dd35247e577fe0cb61c60427d0d665
Mohammad Afzal <mafzal91@gmail.com>
2016-06-17 23:31:56 -0400

    added ip script

--- server.js
+++ server.js
@@ -2,7 +2,15 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-  res.send('Hello World! Running for raspberry Pi');
+  var text ="";
+  for(int i = 0; i < 1999; i++){
+    for(int i = 0; i < 1999; i++){
+      if(i == k){
+        text += i + k;
+      }
+    }
+  }
+  res.send('Hello World! Running for raspberry Pi' + text );
 });
 
 app.listen(3000, function () {
